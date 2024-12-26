25ea61e71691af20b708863162b41b4fbd5e11f3
Hamza Amin <mh4070685@gmail.com>
2024-12-26 16:14:29 +0500

    testing

--- haazri_hub/api/urls.py
+++ haazri_hub/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ObjectDetectionView,get_attendance_by_date
+from .views import ObjectDetectionView, get_attendance_by_date
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('detect-objects/', ObjectDetectionView.as_view(), name='detect-objects'),
+    path('detect-objects/', csrf_exempt(ObjectDetectionView.as_view()), name='detect-objects'),
     path('get-attendance/', get_attendance_by_date, name='get-attendance'),
 ]
